33df04a53470c7c0e97cc952ba8176fd8354e982
Juan Pablo Villanueva <juanpablow@gmail.com>
2023-04-21 00:21:03 -0400

    Programa componente agregar colaborador

--- src/components/AgregarComp.jsx
+++ src/components/AgregarComp.jsx
@@ -1,23 +1,68 @@
+import { useState } from 'react';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import data from './colabs.json';
 
 function Agregar() {
+  const [arregloColabs, setArregloColabs] = useState(data);
+  const [nuevoColabNombre, setNuevoColabNombre] = useState('');
+  const [nuevoColabCorreo, setNuevoColabCorreo] = useState('');
+
+  const handleChange = (e) => {
+    e.target.name === 'nombre'
+      ? setNuevoColabNombre(e.target.value)
+      : e.target.name === 'correo'
+      ? setNuevoColabCorreo(e.target.value)
+      : null;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setArregloColabs([
+      ...arregloColabs,
+      {
+        id: (arregloColabs.length + 1).toString(),
+        nombre: nuevoColabNombre,
+        correo: nuevoColabCorreo,
+      },
+    ]);
+  };
+
   return (
-    <Form className="my-5">
+    <Form
+      className="my-5"
+      onSubmit={(e) => {
+        handleSubmit(e);
+      }}
+    >
       <FloatingLabel
         controlId="floatingName"
         label="Nombre del Colaborador"
         className="mb-3"
       >
-        <Form.Control type="text" placeholder="Nombre del Colaborador" />
+        <Form.Control
+          type="text"
+          placeholder="Nombre del Colaborador"
+          name="nombre"
+          onChange={(e) => {
+            handleChange(e);
+          }}
+        />
       </FloatingLabel>
       <FloatingLabel
         controlId="floatingEmail"
         label="Correo electrónico"
         className="mb-3"
       >
-        <Form.Control type="email" placeholder="Correo electrónico" />
+        <Form.Control
+          type="email"
+          placeholder="Correo electrónico"
+          name="correo"
+          onChange={(e) => {
+            handleChange(e);
+          }}
+        />
       </FloatingLabel>
       <div className="d-grid gap-2">
         <Button variant="primary" type="submit">
--- src/components/ColaboradoresComp.jsx
+++ src/components/ColaboradoresComp.jsx
@@ -7,7 +7,6 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import data from './colabs.json';
 import ListGroup from 'react-bootstrap/ListGroup';
-import { Badge, ListGroupItem } from 'react-bootstrap';
 
 function Colaboradores() {
   const [colabs, setColabs] = useState(data);
@@ -41,18 +40,6 @@ function Colaboradores() {
             <Col></Col>
           </Row>
         </Accordion.Item>
-        {/* <Accordion.Item eventKey="1">
-        <Accordion.Header>Lista de Colaboradores</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item> */}
         <Accordion.Item eventKey="2" className="py-4">
           <h5 className="pt-4 text-center">Lista de Colaboradores</h5>
           <Row>
